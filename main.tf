045c17a4ff079583187855552400ae3c5d8b9b38
Eduardo D <79931269+edu-mrduarte@users.noreply.github.com>
2025-06-23 11:56:17 +0100

    Update main.tf - rm maybe outdated format
    thanks shitty tutorial

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/Terraform_and_AWS_Learning/terraform/s3-webapp/aws"
+  source  = "app.terraform.io/Terraform_and_AWS_Learning/s3-webapp/aws"
   name    = var.name
   region  = var.region
   prefix  = var.prefix
